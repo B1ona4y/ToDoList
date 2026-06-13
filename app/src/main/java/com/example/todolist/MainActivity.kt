a9730336e1261a8aacabf120d7962c70c7ddae4f
B1ona4y <eu.oleynich@gmail.com>
2026-06-13 13:11:56 +0200

    feat: Main activity now is using viewmodel setting for theme

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import com.example.todolist.ui.navigation.NavGraph
 import com.example.todolist.ui.theme.ToDoListTheme
+import com.example.todolist.viewmodel.SettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ToDoListTheme {
+            val settings by settingsViewModel.settings.collectAsState()
+            ToDoListTheme(darkTheme = settings.darkMode) {
                 NavGraph()
             }
         }
